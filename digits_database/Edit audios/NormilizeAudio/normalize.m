652782116aa1f6c2a9d5305efa5b326f5377b65d
Nicolas Zorzano <nico.zorzano@live.com.ar>
2018-02-05 18:11:01 -0300

    Made it more automatic

--- digits_database/Edit audios/NormilizeAudio/normalize.m
+++ digits_database/Edit audios/NormilizeAudio/normalize.m
@@ -2,19 +2,20 @@ clc
 close all
 clear all
 
-
-audio_list = glob("9/*.wav");
-
-for i=1:1:length(audio_list)
-
-	[speech, fs] = wavread( audio_list(i){1} );
+mkdir("converted");
+mkdir("converted/wav");
+for i=0:1:9
+	mkdir(sprintf("converted/wav/%i",i));
+	audio_list = glob(sprintf("wav/%i/*.wav",i));
+	for i=1:1:length(audio_list)
+		[speech, fs] = wavread(audio_list(i){1});
 	
-	val_max = abs(max(speech));
-	val_min = abs(min(speech));
-	factor = max(val_max, val_min);
-	speech /= factor;
-	aux=sprintf('converted/%s',audio_list(i){1});
-	wavwrite(speech,fs,aux);
-
+		val_max = abs(max(speech));
+		val_min = abs(min(speech));
+		factor = max(val_max, val_min);
+		speech /= factor;
+		aux=sprintf('converted/%s',audio_list(i){1});
+		wavwrite(speech,fs,aux);
+	endfor
+	disp('Done');
 endfor
-
