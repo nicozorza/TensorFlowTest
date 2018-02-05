bcc25d5614c096636f497f3920c4623f1dd114a9
Nicolas Zorzano <nico.zorzano@live.com.ar>
2018-02-05 18:27:45 -0300

    Train and test set now are not overwritten in every run

--- src/speech_recognition/spoken_digits_recognition_tflearn.py
+++ src/speech_recognition/spoken_digits_recognition_tflearn.py
@@ -11,21 +11,44 @@ OUT_MODEL = 'NetworkModel'
 # Validate = 1
 # Train and validate = 2
 train_flag = 0
+create_datasets = False
 
+batch_size = 50
 learning_rate = 0.0005
 n_classes = 10
 n_epochs = 100
 
-# Load the database
-file = open(MFCC_DIR+'/'+OUT_FILE, 'rb')
-data = pickle.load(file)
-file.close()
-database = MfccDatabase(data)
+if create_datasets:
+    # Load the database
+    file = open(MFCC_DIR + '/' + OUT_FILE, 'rb')
+    data = pickle.load(file)
+    file.close()
+    database = MfccDatabase(data)
+    # Separate train and test samples
+    train_set, test_set = database.trainTestSet(0.9)
+    # Save train and test sets
+    file = open(MFCC_DIR + '/' + 'train_set', 'wb')
+    # Trim the samples to a fixed length
+    pickle.dump(train_set.print(), file)
+    file.close()
+    file = open(MFCC_DIR + '/' + 'test_set', 'wb')
+    # Trim the samples to a fixed length
+    pickle.dump(test_set.print(), file)
+    file.close()
+else:
+    # Load the database
+    file = open(MFCC_DIR + '/' + 'train_set', 'rb')
+    data = pickle.load(file)
+    file.close()
+    train_set = MfccDatabase(data)
+    file = open(MFCC_DIR+ '/' + 'test_set', 'rb')
+    data = pickle.load(file)
+    file.close()
+    test_set = MfccDatabase(data)
 
 # Get the MFCC matrix size
-n_mfcc = database.getNMfcc()
-n_frames = database.getNFrames()
-batch_size = 50
+n_mfcc = train_set.getNMfcc()
+n_frames = train_set.getNFrames()
 
 # Define the neural network structure
 neural_net = TflearnNeuralNetwork(
@@ -42,35 +65,12 @@ neural_net.conv_neural_network_model()
 
 # Only train
 if train_flag == 0:
-    # Separate train and test samples
-    train_set, test_set = database.trainTestSet(0.9)
-
-    # Save train and test sets
-    file = open(MFCC_DIR + '/' + OUT_MODEL_DIR + '/' + 'train_set', 'wb')
-    # Trim the samples to a fixed length
-    pickle.dump(train_set.print(), file)
-    file.close()
-    file = open(MFCC_DIR + '/' + OUT_MODEL_DIR + '/' + 'test_set', 'wb')
-    # Trim the samples to a fixed length
-    pickle.dump(test_set.print(), file)
-    file.close()
-
     # Train the network
     neural_net.train_neural_network(train_set=train_set)
     neural_net.save_model(MFCC_DIR+'/'+OUT_MODEL_DIR+'/'+OUT_MODEL)
 
 # Only validate
 if train_flag == 1:
-    # Load the database
-    file = open(MFCC_DIR + '/' + OUT_MODEL_DIR + '/' + 'train_set', 'rb')
-    data = pickle.load(file)
-    file.close()
-    train_set = MfccDatabase(data)
-    file = open(MFCC_DIR + '/' + OUT_MODEL_DIR + '/' + 'test_set', 'rb')
-    data = pickle.load(file)
-    file.close()
-    test_set = MfccDatabase(data)
-
     # Load network model
     neural_net.load_model(MFCC_DIR+'/'+OUT_MODEL_DIR+'/'+OUT_MODEL)
 
@@ -79,6 +79,11 @@ if train_flag == 1:
 
 # Train and validate
 if train_flag == 2:
+    # Load the database
+    file = open(MFCC_DIR + '/' + OUT_FILE, 'rb')
+    data = pickle.load(file)
+    file.close()
+    database = MfccDatabase(data)
     # Train and validate the network
     neural_net.train_validate_neural_network(database, 0.1)
     neural_net.save_model(MFCC_DIR + '/' + OUT_MODEL_DIR + '/' + OUT_MODEL)
